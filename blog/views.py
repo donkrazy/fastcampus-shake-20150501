541608b066e9ee89441ca9c6632080e6cc3cfa45
Chinseok Lee <allieuslee@gmail.com>
2015-05-02 19:52:35 +0900

    blog.views.index 뷰의 응답을 html 템플릿을 이용

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('hello blog/index')
+    return render(request, 'blog/index.html', {
+    })
 
