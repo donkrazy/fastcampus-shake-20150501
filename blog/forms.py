ea2e0de0a896eb395bd37eb91e8e83bf9f162bf0
Chinseok Lee <allieuslee@gmail.com>
2015-05-02 22:04:38 +0900

    댓글입력 창의 열수(rows) 를 2줄로 조정

--- blog/forms.py
+++ blog/forms.py
@@ -12,4 +12,7 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('message',)
+        widgets = {
+            'message': forms.Textarea(attrs={'rows': 2}),
+        }
 
