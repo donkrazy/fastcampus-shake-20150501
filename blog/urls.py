527a8d1ce1307bcc5b71bd05ae458a3873339a6a
Chinseok Lee <allieuslee@gmail.com>
2015-05-02 20:10:03 +0900

    Post 수정 구현 (blog.views.edit 뷰)

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('blog.views',  # noqa
     url(r'^$', 'index', name='index'),
     url(r'^new/$', 'new', name='post_new'),
+    url(r'^(?P<id>\d+)/edit/$', 'edit', name='post_edit'),
 )
 
