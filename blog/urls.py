1b28629f135a33d90f2eaccc01577ea6bf071ae4
Chinseok Lee <allieuslee@gmail.com>
2015-05-02 20:41:01 +0900

    댓글수정 구현

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,7 @@ urlpatterns = patterns('blog.views',  # noqa
     url(r'^(?P<id>\d+)/edit/$', 'edit', name='post_edit'),
     url(r'^(?P<id>\d+)/delete/$', 'delete', name='post_delete'),
     url(r'^(?P<id>\d+)/comments/new/$', 'comment_new', name='comment_new'),
+    url(r'^(?P<id>\d+)/comments/(?P<comment_id>\d+)/edit/$', 'comment_edit',
+        name='comment_edit'),
 )
 
