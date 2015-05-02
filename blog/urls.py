9fcff46b9a994abe6c869930c8f6093542abcb57
Chinseok Lee <allieuslee@gmail.com>
2015-05-02 20:30:21 +0900

    post 삭제 구현

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns('blog.views',  # noqa
     url(r'^(?P<id>\d+)/$', 'detail', name='post_detail'),
     url(r'^new/$', 'new', name='post_new'),
     url(r'^(?P<id>\d+)/edit/$', 'edit', name='post_edit'),
+    url(r'^(?P<id>\d+)/delete/$', 'delete', name='post_delete'),
 )
 
