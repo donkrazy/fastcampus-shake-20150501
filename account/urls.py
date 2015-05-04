0a003cc37cc2f2d9841aed60d851cc9c796ad7c8
Chinseok Lee <allieuslee@gmail.com>
2015-05-04 23:47:41 +0900

    인증관련 url 라우팅 처리를 account 앱으로 이관

---
+++ account/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include, url
+from account.forms import QuizAuthenticationForm
+
+urlpatterns = [
+    url(r'^login/$', 'django.contrib.auth.views.login', {
+        'authentication_form': QuizAuthenticationForm,
+    }, name='login'),
+    url(r'', include('django.contrib.auth.urls')),
+]
+
--- shake/urls.py
+++ shake/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import RedirectView
-from auth.forms import QuizAuthenticationForm
 
 urlpatterns = [
     # Examples:
@@ -11,8 +10,5 @@ urlpatterns = [
     url(r'^$', RedirectView.as_view(pattern_name='blog:index'), name='root'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls', namespace='blog')),
-    url(r'^auth/login/$', 'django.contrib.auth.views.login', {
-        'authentication_form': QuizAuthenticationForm,
-    }, name='login'),
-    url(r'^auth/', include('django.contrib.auth.urls')),
+    url(r'^account/', include('account.urls')),
 ]
