74b65edc636f396ab29ccd5d36bbfde96e2cc7a3
Chinseok Lee <allieuslee@gmail.com>
2015-05-05 00:15:04 +0900

    회원가입과 동시에 자동 로그인 처리

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib import messages
+from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.forms import UserCreationForm
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import CreateView
@@ -12,6 +13,11 @@ class SignupView(CreateView):
     success_url = reverse_lazy('root')
 
     def form_valid(self, form):
-        messages.info(self.request, '가입되었습니다. 로그인해주세요.')
-        return super(SignupView, self).form_valid(form)
+        response = super(SignupView, self).form_valid(form)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password1']
+        user = authenticate(username=username, password=password)
+        auth_login(self.request, user)
+        messages.info(self.request, '가입되셨습니다. 환영합니다.')
+        return response
 
