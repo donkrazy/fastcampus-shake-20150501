f797fbaadcf89feadf04dc2d54176e28cbcfb23e
Chinseok Lee <allieuslee@gmail.com>
2015-05-12 17:48:22 +0900

    blog, detail 뷰를 cbv 로 구현

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,10 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('blog.views_cbv',  # noqa
     url(r'^$', 'index', name='index'),
+    url(r'^(?P<id>\d+)/$', 'detail', name='post_detail'),
 )
 
 urlpatterns += patterns('blog.views',  # noqa
-    url(r'^$', 'index', name='index'),
-    url(r'^(?P<id>\d+)/$', 'detail', name='post_detail'),
     url(r'^new/$', 'new', name='post_new'),
     url(r'^(?P<id>\d+)/edit/$', 'edit', name='post_edit'),
     url(r'^(?P<id>\d+)/delete/$', 'delete', name='post_delete'),
