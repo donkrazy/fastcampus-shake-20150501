ddcc3612395a18bf810a6a0bd11643a1856472ff
Chinseok Lee <allieuslee@gmail.com>
2015-05-22 13:24:22 +0900

    django-allauth 의 logout 뷰 대신에 django.contrib.auth 의 logout 뷰 적용

--- shake/urls.py
+++ shake/urls.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import RedirectView
@@ -10,6 +12,11 @@ urlpatterns = [
     url(r'^$', RedirectView.as_view(pattern_name='blog:index'), name='root'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls', namespace='blog')),
+
+    # django-allauth 의 logout 뷰에서는 이메일변경 기능이 있는데,
+    # 일단 이 기능을 노출하지 않기 위해서 적용.
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', name='logout'),
+
     url(r'^accounts/', include('allauth.urls')),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog', name='jsi18n'),
